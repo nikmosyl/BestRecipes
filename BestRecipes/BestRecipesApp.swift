053be83e4606dc9c68fb1089f9f33b9396366ecd
nikita <nikmosyl@gmail.com>
2025-08-10 15:21:06 +0300

    Initial Commit

---
+++ BestRecipes/BestRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestRecipesApp.swift
+//  BestRecipes
+//
+//  Created by nikita on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BestRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
