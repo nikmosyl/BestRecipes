eefbc9a3d624af6950b73910800a813911c8e5ac
Ivan Semikin <i.semikin.94@icloud.com>
2025-08-12 21:09:15 +0200

    Declared properties in CreateRecipeViewModel

--- BestRecipes/Modules/CreateRecipe/ViewModel/CreateRecipeViewModel.swift
+++ BestRecipes/Modules/CreateRecipe/ViewModel/CreateRecipeViewModel.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import PhotosUI
+import _PhotosUI_SwiftUI
 
 final class CreateRecipeViewModel: ObservableObject {
+    @Published var title: String = ""
+    @Published var author: String = ""
+    @Published var readyInMinutes: Int? = nil
+    @Published var servings: Int? = nil
+    @Published var dishTypes: [String] = []
     
+    @Published var instruction: String = ""
+    @Published var instructions: [Instruction] = []
+    
+    @Published var ingredients: [Ingredient] = []
+    
+    @Published var selectedPhotoItem: PhotosPickerItem? = nil
+    @Published var recipeImage: UIImage? = nil
+    @Published var isLoadingImage: Bool = false
 }
