0afb4e6c57174c9b0445786b89dd8af82eebd40d
Ivan Semikin <i.semikin.94@icloud.com>
2025-08-12 20:44:23 +0200

    Created CreateRecipeViewModel

--- BestRecipes/Modules/CreateRecipe/CreateRecipeView.swift
+++ BestRecipes/Modules/CreateRecipe/CreateRecipeView.swift
@@ -6,11 +6,37 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct CreateRecipeView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var uiImage: UIImage?
+    
     var body: some View {
         ScrollView {
-            
+            Group {
+                if let uiImage {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 200)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .overlay(alignment: .topTrailing) {
+                            PhotosPicker(selection: $selectedItem, matching: .images) {
+                                Image(systemName: "camera.on.rectangle")
+                                    .padding(8)
+                                    .background(.ultraThinMaterial, in: Circle())
+                            }
+                            .padding(10)
+                        }
+                        .onChange(of: selectedItem) { newValue in
+                            // TODO: Implement loadImage
+                            print("")
+                        }
+                } else {
+
+                }
+            }
         }
         .navigationTitle("Create Recipe")
         .navigationBarTitleDisplayMode(.inline)
---
+++ BestRecipes/Modules/CreateRecipe/ViewModel/CreateRecipeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CreateRecipeViewModel.swift
+//  BestRecipes
+//
+//  Created by Иван Семикин on 12/08/2025.
+//
+
+import Foundation
+
+final class CreateRecipeViewModel: ObservableObject {
+    
+}
