88a9636b3dc6edb6305239b36713f9b9171dcf12
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2025-08-13 22:24:40 +0500

    add some logic for state changing of bookmark button
    # Conflicts:
    #	BestRecipes/Modules/NetworkTest/NetworkTestView.swift

--- BestRecipes/Modules/NetworkTest/NetworkTestView.swift
+++ BestRecipes/Modules/NetworkTest/NetworkTestView.swift
@@ -14,64 +14,74 @@ struct NetworkTestView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                if viewModel.isLoading {
-                    ProgressView("Загрузка...")
-                        .padding()
-                } else if let error = viewModel.errorMessage {
-                    Text(error)
-                        .foregroundColor(.red)
-                        .padding()
-                } else {
-                    List(viewModel.recipes, id: \.id) { recipe in
-                        RecipeItem(recipe: recipe, showCookingTime: true)
-                        
-//                        HStack(spacing: 12) {
-//                            AsyncImage(url: URL(string: recipe.imageURL ?? "")) { phase in
-//                                switch phase {
-//                                case .empty:
-//                                    ProgressView()
-//                                        .frame(width: 60, height: 60)
-//                                case .success(let image):
-//                                    image.resizable()
-//                                        .scaledToFill()
-//                                        .frame(width: 60, height: 60)
-//                                        .cornerRadius(8)
-//                                case .failure:
-//                                    Image(systemName: "photo")
-//                                        .resizable()
-//                                        .scaledToFit()
-//                                        .frame(width: 60, height: 60)
-//                                        .foregroundColor(.gray)
-//                                @unknown default:
-//                                    EmptyView()
-//                                }
-//                            }
-//                            
-//                            VStack(alignment: .leading, spacing: 4) {
-//                                Text(recipe.title ?? "Без названия")
-//                                    .font(.headline)
-//                                
-//                                if let time = recipe.readyInMinutes {
-//                                    Text("⏱ \(time) мин")
-//                                        .font(.subheadline)
-//                                        .foregroundColor(.secondary)
-//                                }
-//                                
-//                                if let servings = recipe.servings {
-//                                    Text("Порций: \(servings)")
-//                                        .font(.subheadline)
-//                                        .foregroundColor(.secondary)
-//                                }
-//                            }
-//                        }
-//                        .padding(.vertical, 4)
+            ScrollView {
+                VStack {
+                    if viewModel.isLoading {
+                        ProgressView("Загрузка...")
+                            .padding()
+                    } else if let error = viewModel.errorMessage {
+                        Text(error)
+                            .foregroundColor(.red)
+                            .padding()
+                    } else {
+                        ForEach(viewModel.recipes, id: \.id) { recipe in
+                            RecipeItem(
+                                recipe: recipe,
+                                showCookingTime: true,
+                                isBookmarked: viewModel.checkBookmarked(id: recipe.id)
+                            )
+                        }
+                        //                    List(viewModel.recipes, id: \.id) { recipe in
+                        //                        HStack(spacing: 12) {
+                        //                            AsyncImage(url: URL(string: recipe.imageURL ?? "")) { phase in
+                        //                                switch phase {
+                        //                                case .empty:
+                        //                                    ProgressView()
+                        //                                        .frame(width: 60, height: 60)
+                        //                                case .success(let image):
+                        //                                    image.resizable()
+                        //                                        .scaledToFill()
+                        //                                        .frame(width: 60, height: 60)
+                        //                                        .cornerRadius(8)
+                        //                                case .failure:
+                        //                                    Image(systemName: "photo")
+                        //                                        .resizable()
+                        //                                        .scaledToFit()
+                        //                                        .frame(width: 60, height: 60)
+                        //                                        .foregroundColor(.gray)
+                        //                                @unknown default:
+                        //                                    EmptyView()
+                        //                                }
+                        //                            }
+                        //
+                        //                            VStack(alignment: .leading, spacing: 4) {
+                        //                                Text(recipe.title ?? "Без названия")
+                        //                                    .font(.headline)
+                        //
+                        //                                if let time = recipe.readyInMinutes {
+                        //                                    Text("⏱ \(time) мин")
+                        //                                        .font(.subheadline)
+                        //                                        .foregroundColor(.secondary)
+                        //                                }
+                        //
+                        //                                if let servings = recipe.servings {
+                        //                                    Text("Порций: \(servings)")
+                        //                                        .font(.subheadline)
+                        //                                        .foregroundColor(.secondary)
+                        //                                }
+                        //                            }
+                        //                        }
+                        //                        .padding(.vertical, 4)
+                        //                    }
                     }
                 }
-            }
-            .navigationTitle("Рецепты")
-            .task {
-                await viewModel.loadRecipes()
+                .onAppear {
+                    viewModel.fetchBookmarkedRecipes()
+                }
+                .navigationTitle("Рецепты")
+                .task {
+                    await viewModel.loadRecipes()
+                }
             }
         }
     }
