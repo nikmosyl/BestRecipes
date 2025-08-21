a5d72c0339770ddb1ed51e9508280f046d1fab76
nikita <nikmosyl@gmail.com>
2025-08-21 17:47:27 +0400

    добавлен отступ для последней ячейки

--- BestRecipes/Modules/TabBar/TabBarView.swift
+++ BestRecipes/Modules/TabBar/TabBarView.swift
@@ -16,7 +16,9 @@ struct TabBarView: View {
                 viewModel.selectedTab.view
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Color(.systemGroupedBackground))
-                    .ignoresSafeArea(edges: .bottom) // Только bottom, чтобы контент не заезжал под status bar
+                    .safeAreaInset(edge: .bottom) {
+                        Color.clear.frame(height: 60)
+                    }
                 
                 CustomTabBarView(viewModel: viewModel)
             }
